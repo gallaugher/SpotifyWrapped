4cd896e83c0a706f2a40307bb0936aeb32905788
gallaugher <john.gallaugher@bc.edu>
2025-12-04 18:03:16 -0500

    Initial Commit

---
+++ SpotifyWrapped/ContentView.swift
@@ -0,0 +1,22 @@
+//  ContentView.swift
+//  SpotifyWrapped
+//  Created by John Gallaugher on 12/4/25.
+//  YouTube.com/profgallaugher - gallaugher.bsky.social
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SpotifyWrapped/SpotifyWrappedApp.swift
@@ -0,0 +1,15 @@
+//  SpotifyWrappedApp.swift
+//  SpotifyWrapped
+//  Created by John Gallaugher on 12/4/25.
+//  YouTube.com/profgallaugher - gallaugher.bsky.social
+
+import SwiftUI
+
+@main
+struct SpotifyWrappedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
